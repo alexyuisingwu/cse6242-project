442815cc3ac5837ae402edb102b45284521d2ab3
Alex Wu <awu68@gatech.edu>
2017-12-06 23:37:49 -0500

    - root path fix

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def index():
     if request.method == 'POST':
         script_file = request.files['script']
         script_title = script_file.filename.split('.')[0]
-        path = os.path.join(app.root_path, 'static/tmp/{}.csv'.format(script_title))
+        path = os.path.join('static/tmp/{}.csv'.format(script_title))
         with open(path, 'wb') as out_file:
             analyze_external_script(script_file=script_file, output_file=out_file)
         return url_for('static', filename='tmp/{}.csv'.format(script_title))
